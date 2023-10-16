140f0368abd98a1a9a02c8dcae1186f5d8355551
kpmood <kpmood@gmail.com>
2023-10-16 17:28:52 +0700

    #Last changes

--- AEX/Data/MyDataContext.cs
+++ AEX/Data/MyDataContext.cs
@@ -14,25 +14,7 @@ namespace AEX.Data
         public MyDataContext(DbContextOptions<MyDataContext> options)
             :base(options)
         {
-            //Films = new List<Film>();
             Database.EnsureCreated();
-
-
-            //var actor = new Actor() { Name = "Сильвестр", Surname = "Сталлоне", Age = 77 };
-            //Actors.Add(actor);
-            //var genre1 = Genres.FirstOrDefault(x => x.Id == 15 );
-            //var genre2 = Genres.FirstOrDefault(x => x.Id == 17);
-
-            //var film = new Film()
-            //{
-            //    Title = "Рокки",
-            //    Description = "Это описание фильма Рокки"
-            //};
-            //film.actors.Add(actor);
-            //film.genres.Add(genre1);
-            //film.genres.Add(genre2);
-            //Films.Add(film);
-            //SaveChanges();
         }
     }
 }
